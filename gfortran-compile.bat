6e90512af28dd553fa22633d48ebda7eb0e403b2
novvier <novvier@outlook.com.pe>
2019-04-11 00:29:05 -0500

    Bash-win renombrado

--- gfortran-compile.bat
+++ gfortran-compile.bat
@@ -4,4 +4,4 @@ gfortran -c SCREEN3B.FOR
 gfortran -c SCREEN3C.FOR
 @echo Compilando SCREEN3:
 gfortran -o screen3.exe SCREEN3A.o SCREEN3B.o SCREEN3C.o
-@echo Compilación finalizada
+@echo Compilacion finalizada
